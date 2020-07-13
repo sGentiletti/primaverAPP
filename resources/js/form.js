b4326629edd659539961151f85d8d763d78b86ff
sGentiletti <gentiletti.sergio@gmail.com>
2020-07-12 21:24:01 -0300

    se agrega el js para esta funcionalidad

---
+++ resources/js/form.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+
+    const betweenStreetsInput = document.getElementById('between_streets');
+    const formGroupBetweenStreets = betweenStreetsInput.parentNode.parentNode;
+    const grade = document.getElementById('grade');
+
+    const showBetweenStreets = (e) => {
+        const { value } = e.target;
+        const regExp = /(6|7)/g;
+
+        if(regExp.test(value)) {
+            if(formGroupBetweenStreets.classList.contains('d-none')) {
+                formGroupBetweenStreets.classList.remove('d-none');
+                alert('Completa tus Entrecalles. Va a aparecer para completar debajo de tu dirección');
+            }
+        } else {
+            if(!formGroupBetweenStreets.classList.contains('d-none'))
+                formGroupBetweenStreets.classList.add('d-none');
+        }
+    };
+    
+    grade.addEventListener('change', showBetweenStreets);
+
+  };
